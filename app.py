c101413e7d06237f4013b1d71ca2ab158a21b210
InnocentAnyaele <luisnerma545@gmail.com>
2023-03-15 18:02:42 +0000

    changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response, jsonify
 from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader, download_loader
 from werkzeug.utils import secure_filename
 import pickle
@@ -26,70 +26,116 @@ def delete_context(dirName):
 
 @app.route("/")
 def hello_world():
-    # return "<p>Hi</p>"
-    return render_template('home/index.html')
+    return "<p>The Flask API route for GPTContext</p>"
         
 
 
 @app.route('/addContext', methods=['POST'])
 def add_context():
     if request.method == 'POST':
-        files = request.files.getlist('file')
-        dirName = os.path.join('./uploads/', str(uuid.uuid1()))
-        os.makedirs(dirName)
         
-        for file in files:
-            filename = file.filename
-            new_filename = os.path.join(dirName,secure_filename(filename))
-            file.save(new_filename)   
-         
-        t = threading.Thread(target=delete_context, args=(dirName,))
-        t.start()
+        try:
+            files = request.files.getlist('file')
+            indexKey = str(uuid.uuid1())
+            dirName = os.path.join('./uploads/', indexKey)
+            os.makedirs(dirName)
+            
+            for file in files:
+                filename = file.filename
+                new_filename = os.path.join(dirName,secure_filename(filename))
+                file.save(new_filename)   
+            
+            t = threading.Thread(target=delete_context, args=(dirName,))
+            t.start()
+            
+            try:
+                SimpleDirectoryReader = download_loader('SimpleDirectoryReader')
+                loader = SimpleDirectoryReader(dirName, recursive=True)  
+                documents = loader.load_data()
+                index = GPTSimpleVectorIndex(documents)
+                indexPath = os.path.join(dirName,'index.json')
+                index.save_to_disk(indexPath)
+                # response = index.query("What is his hobbies?")
+                # return str(response)
+                response = make_response(indexKey)
+                response.status_code = 200
+                return response
+            
+            except Exception as e:
+                print (e)
+                response = make_response('Could not build index')
+                response.status_code = 500
+                return response
+            
+        except Exception as e:
+            print (e)
+            response = make_response('Could not upload files')
+            response.status_code = 500
+            return response           
         
         
-        SimpleDirectoryReader = download_loader('SimpleDirectoryReader')
-        loader = SimpleDirectoryReader(dirName, recursive=True)  
-        documents = loader.load_data()
-        index = GPTSimpleVectorIndex(documents)
-        indexPath = os.path.join(dirName,'index.json')
-        index.save_to_disk(indexPath)
-        # response = index.query("What is his hobbies?")
-        # return str(response)
-        print (index)
-        return ('index has been built')
+        
     
     
     
 @app.route('/getResponse', methods=['POST'])
 def get_response():
     if request.method == 'POST':
+
         indexKey = request.form['indexKey']
         prompt = request.form['prompt']
         
-        if len(indexKey) < 1:
-            return 'No index has been built yet for this prompt'
-        
-        indexPath = os.path.join('uploads',indexKey,'index.json')
-        
-        print (indexPath)
-        
-        if os.path.exists(indexPath):
-            index = GPTSimpleVectorIndex.load_from_disk(indexPath)
-            response = index.query(prompt)
-            return str(response)
-        else:
-            return 'Index has expired for this prompt'    
+        print (indexKey)        
+        print (prompt)
+                    
+        try:
+            indexKey = request.form['indexKey']
+            prompt = request.form['prompt']
+            
+            if len(indexKey) < 1:
+                response = make_response('No index has been built yet for this prompt')
+                response.status_code = 404
+                return response
+            
+            indexPath = os.path.join('uploads',indexKey,'index.json')
+            
+            print (indexPath)
+            
+            if os.path.exists(indexPath):
+                index = GPTSimpleVectorIndex.load_from_disk(indexPath)
+                res = index.query(prompt)
+                response = make_response(str(res))
+                response.status_code = 200
+                return response
+            else:
+                response = make_response('Index has expired for this prompt')
+                response.status_code = 404
+                return response    
+        except Exception as e:
+            print (e)
+            response = make_response('Something went wrong')
+            response.status_code = 500
+            return response
+            
+            
         
 
 @app.route('/deleteAllContext', methods=['DELETE'])
 def deleteAllContext():
     if request.method == 'DELETE':
+        try:
+            path = './uploads'
+            if os.path.exists(path):
+                shutil.rmtree(path)
+                response = make_response('Folder removed!')
+                response.status_code = 200
+                return response
+        except Exception as e:
+            print (e)
+            response = make_response('Could not delete')
+            response.status_code = 500
+            return response
         
-        
-# @app.route('/deleteContext', methods=[])
-# def delete_context():
-#     pass
-
 if __name__ == '__main__':
     app.run()
     
