2f2dbcd3499191e72ac551e8e4597f79845782af
InnocentAnyaele <luisnerma545@gmail.com>
2023-04-13 12:30:24 +0000

    added csv handler, get filetype from client

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ import threading
 import time
 import shutil
 import keys
-
 from flask_cors import CORS
+
+import pandas as pd
+# from gpt_index.indices.struct_store import GPTPandasIndex
 
 # os.environ['OPENAI_API_KEY'] = 'key-here'
 os.environ['OPENAI_API_KEY'] = keys.OPENAI_API_KEY
@@ -40,8 +42,6 @@ def hello_world():
 @app.route('/api/addContext', methods=['POST'])
 def add_context():
     if request.method == 'POST':
-        print ('reached---------------------------')
-        
         # sending from postman
         # try:
         #     files = request.files.getlist('file')
@@ -53,38 +53,90 @@ def add_context():
         #         filename = file.filename
         #         new_filename = os.path.join(dirName,secure_filename(filename))
         #         file.save(new_filename)   
-
             
-         # sending from the frontend
         try:
             indexKey = str(uuid.uuid1())
             dirName = os.path.join('./uploads/', indexKey)
             os.makedirs(dirName)
+            fileNamesArray = []
             print ('this is the file length', request.form['fileLength'])
             for i in range(int(request.form['fileLength'])):
                 currFileName = 'file'+str(i)
                 print ('currFileName', currFileName)
                 currFile = request.files[currFileName]
                 filename = currFile.filename
-                new_filename = os.path.join(dirName,secure_filename(filename))
+                secureFileName = secure_filename(filename)
+                new_filename = os.path.join(dirName,secureFileName)
+                fileNamesArray.append(filename)
                 currFile.save(new_filename)  
             
             t = threading.Thread(target=delete_context, args=(dirName,))
             t.start()
             
             
-            try:
-                SimpleDirectoryReader = download_loader('SimpleDirectoryReader')
-                loader = SimpleDirectoryReader(dirName, recursive=True)  
-                documents = loader.load_data()
+            def checkAndReturnExtension():
+                path = os.path.join(dirName, secure_filename(fileNamesArray[-1]))
+                curr_fileName = fileNamesArray[-1]
+                curr_fileExtension = curr_fileName.split('.')
+                curr_fileExtensionOnly = curr_fileExtension[-1]
+                print ('curr_fileName', curr_fileName)
+                print ('curr_fileExtensionSplit', curr_fileExtension)
+                print ('curr_fileExtensionOnly', curr_fileExtension[-1])
+                return curr_fileExtensionOnly
+            
+            def createIndexAndReturnResponse(documents):
                 index = GPTSimpleVectorIndex(documents)
                 indexPath = os.path.join(dirName,'index.json')
                 index.save_to_disk(indexPath)
-                # response = index.query("What is his hobbies?")
-                # return str(response)
                 response = make_response(indexKey)
                 response.status_code = 200
                 return response
+                 
+            
+            def csvHandler():
+                path = os.path.join(dirName, secure_filename(fileNamesArray[-1]))
+                print ('csv path', path)
+                PandasCSVReader = download_loader("PandasCSVReader")
+                loader = PandasCSVReader()
+                documents = loader.load_data(path)
+                return createIndexAndReturnResponse(documents)
+        
+#             def xlsxHandler():
+#                 path = os.path.join(dirName, secure_filename(fileNamesArray[-1]))
+#                 df = pd.read_excel(path)
+#                 print (df.head())
+#                 index = GPTPandasIndex(df = df).save_to_disk(os.path.join(dirName, 'index.json'))
+# #                 indexResponse = index.query(
+# #     "What colours are in the document and what numbers are in the document?",
+# # )
+# #                 print ('test response', indexResponse)
+#                 response = make_response(indexKey)
+#                 response.status_code = 200
+#                 return response
+                
+
+            def directoryReader():
+                SimpleDirectoryReader = download_loader('SimpleDirectoryReader')
+                loader = SimpleDirectoryReader(dirName, recursive=True)  
+                documents = loader.load_data()
+                return createIndexAndReturnResponse(documents)
+
+            
+            try:
+                # return directoryReader()
+                # return csvHandler()
+                # checkAndReturnExtension()
+                
+                
+                if checkAndReturnExtension() == 'csv':
+                    print ('Handling CSV')
+                    return csvHandler()
+                elif checkAndReturnExtension() == 'xlsx':
+                    print ('Handling XLSX')
+                    return xlsxHandler()
+                else:
+                    print ('Handling Other')
+                    return directoryReader()
             
             except Exception as e:
                 print (e)
@@ -116,6 +168,9 @@ def get_response():
         try:
             indexKey = request.form['indexKey']
             prompt = request.form['prompt']
+            fileType = request.form['fileType']
+            
+            print ('this is the fileType gotten from the frontend', fileType)
             
             if len(indexKey) < 1:
                 response = make_response('No index has been built yet for this prompt')
@@ -128,6 +183,7 @@ def get_response():
             
             if os.path.exists(indexPath):
                 index = GPTSimpleVectorIndex.load_from_disk(indexPath)
+                # index = GPTPandasIndex.load_from_disk(indexPath)
                 res = index.query(prompt)
                 response = make_response(str(res))
                 response.status_code = 200
